ab73073869d0a5309afa994909cb646dff05807c
Margarita-Harutyunyan <107138430+Margarita-Harutyunyan@users.noreply.github.com>
2022-09-28 16:53:03 +0400

    Create kg_to_pounds.py

---
+++ kg_to_pounds.py
@@ -0,0 +1,5 @@
+def kg_to_pounds():
+    '''Converts kilograms to pounds'''
+    kg = float(input('Enter a weight in kilograms: '))
+    pound = kg * 2.2
+    print(f'{kg} kg is equal to {pound} pounds')
